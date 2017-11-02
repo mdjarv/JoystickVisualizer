26e72d98e1e978f9649ba9488be2882b040a5528
Mathias Djärv <mathias.djarv@gmail.com>
2017-11-03 00:57:58 +0100

    first UDP listener draft in Unity along with version upgrade

---
+++ JoystickVisualizer/Assets/JoystickState.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets
+{
+    class JoystickState
+    {
+        public string UsbID;
+        public string Name;
+
+        public Dictionary<string, string> Data;
+
+        public JoystickState(string usbID, string name)
+        {
+            UsbID = usbID;
+            Name = name;
+            Data = new Dictionary<string, string>();
+        }
+
+        internal void Update(string[] message)
+        {
+            for (int i = 2; i < message.Length; i++)
+            {
+                string[] state = message[i].Split('=');
+
+                Data[state[0]] = state[1];
+            }
+
+            // Debug.Log(this.ToString());
+        }
+
+        public override string ToString()
+        {
+            string state = String.Format("State of {0} ({1}): ", Name, UsbID);
+
+            foreach (KeyValuePair<string, string> entry in Data)
+            {
+                state += String.Format("\n * {0} = {1}", entry.Key, entry.Value);
+            }
+
+            return state;
+        }
+    }
+}
---
+++ JoystickVisualizer/Assets/UDPListener.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System;
+using System.Text;
+using Assets;
+
+public class UDPListener : MonoBehaviour {
+
+    public int Port = 11000;
+
+    Dictionary<string, JoystickState> States = new Dictionary<string, JoystickState>();
+
+    private UdpClient listener;
+
+    // Use this for initialization
+    void Start () {
+        listener = new UdpClient(Port);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        try
+        {
+            while (listener.Available > 0)
+            {
+                IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, Port);
+                Byte[] recieveBytes = listener.Receive(ref groupEP);
+                string[] message = Encoding.ASCII.GetString(recieveBytes).Split(',');
+
+                if (!States.ContainsKey(message[0]))
+                {
+                    States.Add(message[0], new JoystickState(message[0], message[1]));
+                }
+
+                States[message[0]].Update(message);
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Failed to read from UDP socket");
+        }
+    }
+}
