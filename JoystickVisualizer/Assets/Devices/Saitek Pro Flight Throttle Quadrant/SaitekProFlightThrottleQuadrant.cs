d951735a04971fd8984649e2092c7f2e54354d7f
Mathias Djärv <mathias.djarv@gmail.com>
2018-03-08 21:13:36 +0100

    preparations for Saitek Pro Flight Throttle Quadrant

---
+++ JoystickVisualizer/Assets/Devices/Saitek Pro Flight Throttle Quadrant/SaitekProFlightThrottleQuadrant.cs
@@ -0,0 +1,67 @@
+﻿using Assets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaitekProFlightThrottleQuadrant : MonoBehaviour {
+    public const string USB_ID = "06a3:0c2d";
+    //public const string USB_ID = "044f:0404";
+
+    public GameObject Model;
+
+    public GameObject Throttle;
+    public GameObject Mixture;
+    public GameObject Flaps;
+
+    // Use this for initialization
+    void Start()
+    {
+        UDPListener.StickEventListener += StickEvent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void StickEvent(JoystickState state)
+    {
+        if (state.UsbID != USB_ID)
+        {
+            return;
+        }
+
+        Model.SetActive(true);
+
+        foreach (KeyValuePair<string, int> entry in state.Data)
+        {
+            switch (entry.Key)
+            {
+                case "Connected":
+                    if (Model.activeInHierarchy)
+                        Model.SetActive(entry.Value == 1);
+                    break;
+
+                case "X":
+                    Throttle.transform.localEulerAngles = new Vector3(ConvertRange(entry.Value, 0, 65535, 40, 0), 0, 0);
+                    break;
+                case "Y":
+                    Mixture.transform.localEulerAngles = new Vector3(ConvertRange(entry.Value, 0, 65535, 40, 0), 0, 0);
+                    break;
+                case "Z":
+                    Flaps.transform.localEulerAngles = new Vector3(ConvertRange(entry.Value, 0, 65535, 40, 0), 0, 0);
+                    break;
+            }
+        }
+    }
+
+    public static float ConvertRange(
+        double value, // value to convert
+        double originalStart, double originalEnd, // original range
+        double newStart, double newEnd) // desired range
+    {
+        double scale = (double)(newEnd - newStart) / (originalEnd - originalStart);
+        return (float)(newStart + ((value - originalStart) * scale));
+    }
+
+}
