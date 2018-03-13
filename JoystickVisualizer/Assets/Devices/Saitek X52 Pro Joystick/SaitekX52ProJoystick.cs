dbd81d3d41d430046fc31a0edf60c4156c81f84c
Mathias Djärv <mathias.djarv@gmail.com>
2018-03-13 20:27:00 +0100

    add x52 pro hotas

---
+++ JoystickVisualizer/Assets/Devices/Saitek X52 Pro Joystick/SaitekX52ProJoystick.cs
@@ -0,0 +1,53 @@
+﻿using Assets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaitekX52ProJoystick : MonoBehaviour {
+    public const string USB_ID = "06a3:0762";
+    public GameObject Model;
+
+    public GameObject Joystick;
+
+    // Use this for initialization
+    void Start () {
+        UDPListener.StickEventListener += StickEvent;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void StickEvent(JoystickState state)
+    {
+        if (state.UsbID != USB_ID)
+        {
+            return;
+        }
+
+        foreach (KeyValuePair<string, int> entry in state.Data)
+        {
+            switch (entry.Key)
+            {
+                case "X":
+                    Model.SetActive(true);
+                    Joystick.transform.eulerAngles = new Vector3(Joystick.transform.eulerAngles.x, Joystick.transform.eulerAngles.y, ConvertRange(entry.Value, 0, 65535, 20, -20));
+                    break;
+                case "Y":
+                    Model.SetActive(true);
+                    Joystick.transform.eulerAngles = new Vector3(ConvertRange(entry.Value, 0, 65535, 20, -20), Joystick.transform.eulerAngles.y, Joystick.transform.eulerAngles.z);
+                    break;
+            }
+        }
+    }
+
+    public static float ConvertRange(
+        double value, // value to convert
+        double originalStart, double originalEnd, // original range
+        double newStart, double newEnd) // desired range
+    {
+        double scale = (double)(newEnd - newStart) / (originalEnd - originalStart);
+        return (float)(newStart + ((value - originalStart) * scale));
+    }
+}
---
+++ JoystickVisualizer/Assets/Devices/Saitek X52 Pro Throttle/SaitekX52ProThrottle.cs
@@ -0,0 +1,56 @@
+﻿using Assets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaitekX52ProThrottle : MonoBehaviour {
+    public const string USB_ID = "06a3:0762";
+    private const float FLIP_SWITCH_ROTATION = 20.0f;
+    public GameObject Model;
+
+    public GameObject GimbalLeft;
+    public GameObject GimbalRight;
+
+    // Use this for initialization
+    void Start()
+    {
+        UDPListener.StickEventListener += StickEvent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void StickEvent(JoystickState state)
+    {
+        if (state.UsbID != USB_ID)
+        {
+            return;
+        }
+
+        foreach (KeyValuePair<string, int> entry in state.Data)
+        {
+            switch (entry.Key)
+            {
+                case "Z": // Throttle
+                    Model.SetActive(true);
+                    
+                    // Rotate X between -30 and 30
+                    GimbalLeft.transform.eulerAngles = new Vector3(ConvertRange(entry.Value, 0, 65535, 40, -25), GimbalLeft.transform.eulerAngles.y, GimbalLeft.transform.eulerAngles.z);
+                    GimbalRight.transform.eulerAngles = new Vector3(ConvertRange(entry.Value, 0, 65535, 40, -25), GimbalRight.transform.eulerAngles.y, GimbalRight.transform.eulerAngles.z);
+                    break;
+            }
+        }
+    }
+
+    public static float ConvertRange(
+        double value, // value to convert
+        double originalStart, double originalEnd, // original range
+        double newStart, double newEnd) // desired range
+    {
+        double scale = (double)(newEnd - newStart) / (originalEnd - originalStart);
+        return (float)(newStart + ((value - originalStart) * scale));
+    }
+
+}
