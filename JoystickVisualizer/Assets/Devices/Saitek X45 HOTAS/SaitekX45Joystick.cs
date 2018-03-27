65e0f5faa89b78378eb2a9732ebdef869e2e735f
Mathias Djärv <mathias.djarv@gmail.com>
2018-03-27 21:25:27 +0200

    add saitek x45 with models from x52 throttle and warthog stick

---
+++ JoystickVisualizer/Assets/Devices/Saitek X45 HOTAS/SaitekX45Joystick.cs
@@ -0,0 +1,56 @@
+﻿using Assets;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaitekX45Joystick : MonoBehaviour {
+    public const string USB_ID = "06a3:2541";
+    //public const string USB_ID = "044f:0402";
+
+    public GameObject Model;
+
+    public GameObject Joystick;
+
+    // Use this for initialization
+    void Start()
+    {
+        UDPListener.StickEventListener += StickEvent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void StickEvent(JoystickState state)
+    {
+        if (state.UsbID != USB_ID)
+        {
+            return;
+        }
+        Model.SetActive(true);
+
+        foreach (KeyValuePair<string, int> entry in state.Data)
+        {
+            switch (entry.Key)
+            {
+                case "X":
+                    Joystick.transform.eulerAngles = new Vector3(Joystick.transform.eulerAngles.x, Joystick.transform.eulerAngles.y, ConvertRange(entry.Value, 0, 65535, 20, -20));
+                    break;
+                case "Y":
+                    Joystick.transform.eulerAngles = new Vector3(ConvertRange(entry.Value, 0, 65535, 20, -20), Joystick.transform.eulerAngles.y, Joystick.transform.eulerAngles.z);
+                    break;
+            }
+        }
+    }
+
+    public static float ConvertRange(
+        double value, // value to convert
+        double originalStart, double originalEnd, // original range
+        double newStart, double newEnd) // desired range
+    {
+        double scale = (double)(newEnd - newStart) / (originalEnd - originalStart);
+        return (float)(newStart + ((value - originalStart) * scale));
+    }
+
+}
+
---
+++ JoystickVisualizer/Assets/Devices/Saitek X45 HOTAS/SaitekX45Throttle.cs
@@ -0,0 +1,53 @@
+﻿using Assets;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaitekX45Throttle : MonoBehaviour {
+    public const string USB_ID = "06a3:2541";
+    //public const string USB_ID = "044f:0404";
+
+    public GameObject Model;
+
+    public GameObject Throttle;
+
+    // Use this for initialization
+    void Start()
+    {
+        UDPListener.StickEventListener += StickEvent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void StickEvent(JoystickState state)
+    {
+        if (state.UsbID != USB_ID)
+        {
+            return;
+        }
+        Model.SetActive(true);
+
+        foreach (KeyValuePair<string, int> entry in state.Data)
+        {
+            switch (entry.Key)
+            {
+                case "Sliders0": // Throttle
+                    Throttle.transform.localEulerAngles = new Vector3(ConvertRange(entry.Value, 0, 65535, -25, 28), Throttle.transform.localEulerAngles.y, Throttle.transform.localEulerAngles.z);
+                    break;
+            }
+        }
+    }
+
+    public static float ConvertRange(
+        double value, // value to convert
+        double originalStart, double originalEnd, // original range
+        double newStart, double newEnd) // desired range
+    {
+        double scale = (double)(newEnd - newStart) / (originalEnd - originalStart);
+        return (float)(newStart + ((value - originalStart) * scale));
+    }
+
+}
+
