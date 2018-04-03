c3d0af841aef7ba7dfceba7c529bed343c011823
Mathias Djärv <mathias.djarv@gmail.com>
2018-04-03 18:58:42 +0200

    add Thrustmaster T.Flight Stick X with Logitech 3D Pro model

---
+++ JoystickVisualizer/Assets/Devices/Thrustmaster T.Flight Stick X/ThrustmasterTFlightStickX.cs
@@ -0,0 +1,68 @@
+﻿using Assets;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrustmasterTFlightStickX : MonoBehaviour
+{
+    public const string USB_ID = "044f:b106";
+    //public const string USB_ID = "044f:0404";
+
+    public GameObject Model;
+    public GameObject Joystick;
+    public GameObject Throttle;
+
+    // Use this for initialization
+    void Start()
+    {
+        UDPListener.StickEventListener += StickEvent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void StickEvent(JoystickState state)
+    {
+        if (state.UsbID != USB_ID)
+        {
+            return;
+        }
+
+        Model.SetActive(true);
+
+        foreach (KeyValuePair<string, int> entry in state.Data)
+        {
+            switch (entry.Key)
+            {
+                case "Connected":
+                    if (Model.activeInHierarchy)
+                        Model.SetActive(entry.Value == 1);
+                    break;
+
+                case "X":
+                    Joystick.transform.localEulerAngles = new Vector3(Joystick.transform.localEulerAngles.x, ConvertRange(entry.Value, 0, 65535, -20, 20), Joystick.transform.localEulerAngles.z);
+                    break;
+                case "Y":
+                    Joystick.transform.localEulerAngles = new Vector3(ConvertRange(entry.Value, 0, 65535, 20, -20), Joystick.transform.localEulerAngles.y, Joystick.transform.localEulerAngles.z);
+                    break;
+                case "RotationZ":
+                    Joystick.transform.localEulerAngles = new Vector3(Joystick.transform.localEulerAngles.x, Joystick.transform.localEulerAngles.y, ConvertRange(entry.Value, 0, 65535, -30, 30));
+                    break;
+
+                case "Sliders0":
+                    Throttle.transform.localEulerAngles = new Vector3(ConvertRange(entry.Value, 0, 65535, 30, -85), Throttle.transform.localEulerAngles.y, Throttle.transform.localEulerAngles.z);
+                    break;
+            }
+        }
+    }
+
+    public static float ConvertRange(
+        double value, // value to convert
+        double originalStart, double originalEnd, // original range
+        double newStart, double newEnd) // desired range
+    {
+        double scale = (double)(newEnd - newStart) / (originalEnd - originalStart);
+        return (float)(newStart + ((value - originalStart) * scale));
+    }
+}
